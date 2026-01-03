2c901019bafa18f85ab10acc42706220bdf5ee1e
RiyaRathod5256 <rr1315576@gmail.com>
2026-01-03 15:37:14 +0530

    deleted virtual environment

--- backend/venv/db.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-def get_db_connection():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Riya@1234",
-        database="application_tracker"
-    )
